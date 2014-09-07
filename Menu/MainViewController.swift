942f1c47aa4ccdcc8eabcf079340d2e04db96060
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-07 15:38:35 -0400

    First commit
    Basic storyboard layout and assets in place

---
+++ Menu/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  Menu
+//
+//  Created by Mathew Sanders on 9/7/14.
+//  Copyright (c) 2014 Mat. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func unwindToMainViewController (sender: UIStoryboardSegue){
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ Menu/MenuViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Menu
+//
+//  Created by Mathew Sanders on 9/7/14.
+//  Copyright (c) 2014 Mat. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
